d1012ea380d9c4de26c890165998eba88c3f56cb
garasubo <garasubo@gmail.com>
2022-10-13 20:26:03 +0900

    change instance setting

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,7 @@ module "github-runner" {
   source  = "philips-labs/github-runner/aws"
   version = "1.11.0"
 
+  create_service_linked_role_spot = true
   aws_region = local.aws_region
   vpc_id                          = module.vpc.vpc_id
   subnet_ids                      = module.vpc.private_subnets
@@ -27,5 +28,16 @@ module "github-runner" {
   runner_binaries_syncer_lambda_zip = "lambdas-download/runner-binaries-syncer.zip"
   runners_lambda_zip                = "lambdas-download/runners.zip"
   enable_organization_runners = true
+  runner_extra_labels = "aws"
   instance_types = ["t4g.medium"]
+
+  # override delay of events in seconds
+  delay_webhook_event   = 5
+  runners_maximum_count = 2
+
+  idle_config = [{
+    cron      = "* * 20-23 * * 1-5"
+    timeZone  = "Asia/Tokyo"
+    idleCount = 1
+  }]
 }
